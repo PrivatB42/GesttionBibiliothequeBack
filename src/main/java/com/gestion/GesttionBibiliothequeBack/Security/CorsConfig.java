cb29b565be0f04a5a81cd802e5cd8a74e718b4a7
binde <bindeprivat42@gmail.com>
2023-10-25 13:36:30 +0000

    gte

--- src/main/java/com/gestion/GesttionBibiliothequeBack/Security/CorsConfig.java
+++ src/main/java/com/gestion/GesttionBibiliothequeBack/Security/CorsConfig.java
@@ -1,3 +1,4 @@
+/*
 package com.gestion.GesttionBibiliothequeBack.Security;
 
 import org.springframework.context.annotation.Bean;
@@ -20,3 +21,4 @@ public class CorsConfig {
         };
     }
 }
+*/
